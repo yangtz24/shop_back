07f8ec63059223d341535a501d40ee6ce752d5b2
ytz <yangtz0213@163.com>
2020-06-05 15:17:10 +0800

    脚本修改

--- jenkins/shop-back-jenkins.sh
+++ jenkins/shop-back-jenkins.sh
@@ -1,21 +1,31 @@
 #!/usr/bin/env bash
-app_name='shop'
-project_id='basketboy'
-docker_registry='123.56.143.183:5000'
-container_name='shop'
+APP_NAME='shop-back'
+PROJECT_ID='basketboy'
+#docker_registry='123.56.143.183:5000'
+#container_name='shop'
 # image_tag=${docker_registry}/${project_id}:${POM_VERSION}
 # docker tag ${project_id}:${POM_VERSION} ${image_tag}
 # echo '-------rmi Docker Repository------'
 # docker rmi ${image_tag}
 # docker rmi ${project_id}:${POM_VERSION}
-echo '-------pull Docker Repository------'
-docker pull ${docker_registry}/basketboy/${container_name}
-docker stop ${app_name}
+#echo '-------pull Docker Repository------'
+#docker pull ${docker_registry}/basketboy/${container_name}
+echo '----------select container----------'
+if docker ps -a | grep ${APP_NAME}; then
 echo '---------stop container---------'
-docker rm ${app_name}
+docker stop ${APP_NAME}
 echo '---------rm container-----------'
-docker run -p 8080:8888 --network shop --name ${app_name} \
+docker rm ${APP_NAME}
+echo '---------container end--------'
+fi
+#echo '--------select image----------'
+#if docker images -a | grep ${PROJECT_ID}/shop:0.0.1-SNAPSHOT; then
+#echo '-----rm images--------'
+#docker rmi ${PROJECT_ID}/shop:0.0.1-SNAPSHOT 
+
+echo '---------run container----------'
+docker run -p 8080:8888 --network shop --name ${APP_NAME} \
 -v /etc/localtime:/etc/localtime \
--v /opt/app/${app_name}/logs:/var/logs \
--d ${project_id}/${app_name}:0.0.1
+-v /opt/app/${APP_NAME}/logs:/var/logs \
+-d ${PROJECT_ID}/shop:0.0.1-SNAPSHOT
 echo '----start container----'
